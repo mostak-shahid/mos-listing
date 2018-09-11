aad806736f64a93cb05b7be6ac9a87e4d52fa8ac
Md. Mostak Shahid <mostak.shahid@gmail.com>
2018-09-12 00:44:25 +0600

    1.0.0

--- mos-listing.php
+++ mos-listing.php
@@ -3,7 +3,7 @@
 Plugin Name: Mos Listing from XML
 
 Description: This is a Plugin for Guardian Group Realty Property Listing.
-Version: 1.0
+Version: 1.0.0
 Author: Md. Mostak Shahid
 Author URI: http://URI_Of_The_Plugin_Author
 License: GPL2
